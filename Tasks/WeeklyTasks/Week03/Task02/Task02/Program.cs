590756f8976414dfea99c986386e55cd7d49434e
Naghi Aliyev <naghi.aliyev@gmail.com>
2021-06-20 14:18:36 +0400

    Week03 Task02 done

---
+++ Tasks/WeeklyTasks/Week03/Task02/Task02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int number = 0;
+            do
+            {
+                Console.Write("Eded daxil edin :");
+                number = Convert.ToInt32(Console.ReadLine());
+                sum += number;
+            } while (number != 0);
+            Console.WriteLine($"Daxil olunan ededlerin cemi: {sum}");
+        }
+    }
+}
