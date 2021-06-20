660dc2de5dcb262f6152eecda3ca9481fe287328
Naghi Aliyev <naghi.aliyev@gmail.com>
2021-06-20 14:11:36 +0400

    Week03 Task01 done

---
+++ Tasks/WeeklyTasks/Week03/Task01/Task01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int sum = 0;
+            while (i <=100)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                }
+                i++;
+            }
+            Console.WriteLine($"1-den 100-e qeder cut ededlerin cemi: {sum}");
+        }
+    }
+}
