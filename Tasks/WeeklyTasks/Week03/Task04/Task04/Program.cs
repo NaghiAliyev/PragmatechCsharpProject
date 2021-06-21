3b72003d7620d82ccd0a541059c2a5d4f791ea7c
Naghi Aliyev <naghi.aliyev@gmail.com>
2021-06-21 22:03:03 +0400

    Week03 Task04

---
+++ Tasks/WeeklyTasks/Week03/Task04/Task04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Eded daxil edin :");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int temp = number;
+            int factorial = 1;
+            if (temp == 0)
+            {
+                Console.Write("\nDaxil edilen eded 0 ola bilmez :");
+                number = Convert.ToInt32(Console.ReadLine());
+                temp = number;
+            }
+
+            do
+            {
+                factorial *= temp--;
+            } while (temp > 0) ;
+
+            if (factorial != 0)
+            {
+                Console.WriteLine($"Daxil edilen {number} ededinin faktoriali = {factorial}");
+            }
+        }
+    }
+}
