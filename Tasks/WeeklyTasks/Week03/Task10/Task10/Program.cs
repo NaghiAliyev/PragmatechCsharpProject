b05e061f459943771025d7f9b58cde8cee522c9a
Naghi Aliyev <naghi.aliyev@gmail.com>
2021-06-23 22:05:12 +0400

    Week03 Task10 done

---
+++ Tasks/WeeklyTasks/Week03/Task10/Task10/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Method 01 - with ASCII table
+            int startIndex = 65;
+            //int startIndex = 97; 
+            int endIndex = 90;
+            //int endIndex = 122;
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                if (i!=endIndex)
+                {
+                    Console.Write((char)i + ", ");
+
+                }
+                else
+                {
+                    Console.WriteLine((char)i);
+
+                }
+            }
+
+            // Method 02
+            char startPoint = 'a';
+            //char startPoint = 'A';
+            char endPoint = 'z';
+            //char startPoint = 'Z';
+            for (char ch = startPoint; ch <= endPoint; ch++)
+            {
+                if (ch != endPoint)
+                {
+                    Console.Write(ch + ", ");
+
+                }
+                else
+                {
+                    Console.WriteLine(ch);
+
+                }
+            }
+        }
+    }
+}
