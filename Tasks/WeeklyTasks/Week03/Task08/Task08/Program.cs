31b87eb184a7bf0923bdc1a816928985d62cde33
Naghi Aliyev <naghi.aliyev@gmail.com>
2021-06-23 22:03:59 +0400

    Week03 Task08 done

---
+++ Tasks/WeeklyTasks/Week03/Task08/Task08/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = -1;
+            }
+            Random random = new Random();
+            int counter = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int randomIndex = random.Next(10);
+                while (Array.IndexOf(arr, randomIndex) != -1)
+                {
+                    randomIndex = random.Next(10);
+                    counter++;
+                }
+                arr[i] = randomIndex;
+            }
+            Console.Write("Array-in elementleri: ");
+            foreach (int number in arr)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.WriteLine($"\nArray yaradilan zaman tekrarlanmanin qarsisinin alindigi hallarin sayi: {counter}");
+        }
+    }
+}
