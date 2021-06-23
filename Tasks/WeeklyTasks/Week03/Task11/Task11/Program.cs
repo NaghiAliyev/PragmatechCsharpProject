c535dce2f3d59e91367415f41e20b00701e80388
Naghi Aliyev <naghi.aliyev@gmail.com>
2021-06-23 22:06:01 +0400

    Week03 Task11 done

---
+++ Tasks/WeeklyTasks/Week03/Task11/Task11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 2; number <= 100; number++)
+            {
+                bool f = true;
+                for (int i = 2; i <= number/2; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        f = false;
+                        break;
+                    }
+                }
+                if (f)
+                {
+                    Console.WriteLine($"{number} ededi sade ededdir");
+                }
+            }
+        }
+    }
+}
