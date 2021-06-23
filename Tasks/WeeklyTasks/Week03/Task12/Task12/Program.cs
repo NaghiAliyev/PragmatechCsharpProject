f885339ce3d67f8f591e2e9bf5c3d794b9366936
Naghi Aliyev <naghi.aliyev@gmail.com>
2021-06-23 22:06:23 +0400

    Week03 Task12 done

---
+++ Tasks/WeeklyTasks/Week03/Task12/Task12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iki eded daxil edin: ");
+            Console.Write("Birinci ededi daxil edin: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ikinci ededi daxil edin: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            
+            for (int number = number1; number <= number2; number++)
+            {
+                bool f = true;
+                for (int i = 2; i <= number / 2; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        f = false;
+                        break;
+                    }
+                }
+                if (f)
+                {
+                    Console.WriteLine($"{number} ededi sade ededdir");
+                }
+            }
+        }
+    }
+}
