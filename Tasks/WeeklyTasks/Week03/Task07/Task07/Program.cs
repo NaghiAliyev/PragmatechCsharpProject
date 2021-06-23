70da9403deb6bd1f9f2fa52ac69c4656d4a29077
Naghi Aliyev <naghi.aliyev@gmail.com>
2021-06-23 22:00:43 +0400

    Week03 Task07 done

---
+++ Tasks/WeeklyTasks/Week03/Task07/Task07/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vowels = {"a", "e", "i", "o", "u"};
+            Console.WriteLine("Bir soz daxil edin: ");
+            string word = Console.ReadLine();
+            int count = 0;
+            foreach (string vowel in vowels)
+            {
+                if (word.Contains(vowel))
+                {
+                    // Tekrarlanan saitlerin tapilmasi ucun edilib, bu hissede
+                    // IndexOf methodu ucun mueyyen bir startIndex anlayisi ile internetde qarsilasmisam
+                    int minIndex = word.IndexOf(vowel,0);
+                    while (minIndex != -1)
+                    {
+                        minIndex = word.IndexOf(vowel, minIndex+1);
+                        count++;
+                    }
+                }
+            }
+            if (count != 0)
+            {
+                Console.WriteLine($"{word} sozu daxilinde {count} sayda sait var");
+            }
+            else
+            {
+                Console.WriteLine($"{word} sozu daxilinde sait yoxdur");
+            }
+        }
+    }
+}
