9c9475a8578f568d878789a799a3fbe31067aca2
Naghi Aliyev <naghi.aliyev@gmail.com>
2021-07-02 11:14:46 +0400

    Week04 Task04 done

---
+++ Tasks/WeeklyTasks/Week04/Task05/Task05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Task05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 4, 5, 8, 15, 18, 23, 26, 33, 38, 39};
+            int[] oddNumbers = new int[arr.Length];
+            int index = 0;
+            foreach (int item in arr)
+            {
+                if (item % 2 == 1)
+                {
+                    oddNumbers[index++] = item;
+                }
+            }
+            Array.Resize( ref oddNumbers, index);
+            Console.WriteLine($"Array daxilindeki tek ededlerin sayi = {index}\nEdedler:");
+            foreach (var item in oddNumbers)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
---
+++ Tasks/WeeklyTasks/Week04/Task06/Task06/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Task06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, -4, 27, 0 , 44, -2, 47, 59, -88};
+
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+
+            // Method 01
+            int min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine($"\nArrayin en kicik deyere sahib elementi : {min}");
+
+            // Method 02
+            int minimum = arr.Min();
+            Console.WriteLine($"Arrayin en kicik deyere sahib elementi : {minimum}");
+        }
+    }
+}
