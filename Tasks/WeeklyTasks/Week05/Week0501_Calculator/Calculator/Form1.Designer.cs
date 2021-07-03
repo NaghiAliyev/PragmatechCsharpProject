f2f0944fc0ca12a78b6bf1a65e819597a8106411
Naghi Aliyev <naghi.aliyev@gmail.com>
2021-07-03 19:17:22 +0400

    Calculator Task

---
+++ Tasks/WeeklyTasks/Week05/Week0501_Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Calculator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblSelectOperations = new System.Windows.Forms.Label();
+            this.nudNumber01 = new System.Windows.Forms.NumericUpDown();
+            this.nudNumber02 = new System.Windows.Forms.NumericUpDown();
+            this.cmbOperations = new System.Windows.Forms.ComboBox();
+            this.btnGetResult = new System.Windows.Forms.Button();
+            this.lblOperation = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber02)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(335, 28);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(30, 33);
+            this.lblResult.TabIndex = 0;
+            this.lblResult.Text = "0";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSelectOperations
+            // 
+            this.lblSelectOperations.AutoSize = true;
+            this.lblSelectOperations.Location = new System.Drawing.Point(48, 123);
+            this.lblSelectOperations.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblSelectOperations.Name = "lblSelectOperations";
+            this.lblSelectOperations.Size = new System.Drawing.Size(138, 21);
+            this.lblSelectOperations.TabIndex = 1;
+            this.lblSelectOperations.Text = "Emeliyyati secin :";
+            // 
+            // nudNumber01
+            // 
+            this.nudNumber01.Location = new System.Drawing.Point(246, 116);
+            this.nudNumber01.Name = "nudNumber01";
+            this.nudNumber01.Size = new System.Drawing.Size(120, 27);
+            this.nudNumber01.TabIndex = 2;
+            // 
+            // nudNumber02
+            // 
+            this.nudNumber02.Location = new System.Drawing.Point(246, 200);
+            this.nudNumber02.Name = "nudNumber02";
+            this.nudNumber02.Size = new System.Drawing.Size(120, 27);
+            this.nudNumber02.TabIndex = 2;
+            // 
+            // cmbOperations
+            // 
+            this.cmbOperations.FormattingEnabled = true;
+            this.cmbOperations.Location = new System.Drawing.Point(35, 158);
+            this.cmbOperations.Name = "cmbOperations";
+            this.cmbOperations.Size = new System.Drawing.Size(162, 29);
+            this.cmbOperations.TabIndex = 3;
+            this.cmbOperations.SelectedIndexChanged += new System.EventHandler(this.cmbOperations_SelectedIndexChanged);
+            // 
+            // btnGetResult
+            // 
+            this.btnGetResult.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetResult.Location = new System.Drawing.Point(270, 263);
+            this.btnGetResult.Name = "btnGetResult";
+            this.btnGetResult.Size = new System.Drawing.Size(75, 36);
+            this.btnGetResult.TabIndex = 4;
+            this.btnGetResult.Text = "=";
+            this.btnGetResult.UseVisualStyleBackColor = true;
+            this.btnGetResult.Click += new System.EventHandler(this.btnGetResult_Click);
+            // 
+            // lblOperation
+            // 
+            this.lblOperation.AutoSize = true;
+            this.lblOperation.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperation.Location = new System.Drawing.Point(288, 154);
+            this.lblOperation.Name = "lblOperation";
+            this.lblOperation.Size = new System.Drawing.Size(33, 33);
+            this.lblOperation.TabIndex = 5;
+            this.lblOperation.Text = "^";
+            this.lblOperation.Visible = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 340);
+            this.Controls.Add(this.lblOperation);
+            this.Controls.Add(this.btnGetResult);
+            this.Controls.Add(this.cmbOperations);
+            this.Controls.Add(this.nudNumber02);
+            this.Controls.Add(this.nudNumber01);
+            this.Controls.Add(this.lblSelectOperations);
+            this.Controls.Add(this.lblResult);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber02)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblSelectOperations;
+        private System.Windows.Forms.NumericUpDown nudNumber01;
+        private System.Windows.Forms.NumericUpDown nudNumber02;
+        private System.Windows.Forms.ComboBox cmbOperations;
+        private System.Windows.Forms.Button btnGetResult;
+        private System.Windows.Forms.Label lblOperation;
+    }
+}
+
---
+++ Tasks/WeeklyTasks/Week05/Week0501_Calculator/Calculator/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        string operation;
+        int x;
+        private void Main_Load(object sender, EventArgs e)
+        {
+            // Set Operations to ComboBox
+            cmbOperations.Items.Add("+");
+            cmbOperations.Items.Add("-");
+            cmbOperations.Items.Add("/");
+            cmbOperations.Items.Add("*");
+            cmbOperations.Items.Add("%");
+            x = lblResult.Location.X + lblResult.Size.Width;
+        }
+
+        private void btnGetResult_Click(object sender, EventArgs e)
+        {
+            if (cmbOperations.SelectedItem == null)
+            {
+                MessageBox.Show("Zehmet olmasa emeliyyati secin!", "Xeta", MessageBoxButtons.OK);
+            }
+            else
+            {
+                operation = cmbOperations.SelectedItem.ToString();
+                int number01 = Convert.ToInt32(nudNumber01.Value);
+                int number02 = Convert.ToInt32(nudNumber02.Value);
+                switch (operation)
+                {
+                    case "+":
+                        lblResult.Text = (number01 + number02).ToString();
+                        break;
+                    case "-":
+                        lblResult.Text = (number01 - number02).ToString();
+                        break;
+                    case "*":
+                        lblResult.Text = (number01 * number02).ToString();
+                        break;
+                    case "%":
+                        lblResult.Text = (number01 % number02).ToString();
+                        break;
+                    case "/":
+                        if (number02 == 0)
+                        {
+                            MessageBox.Show("0-a bolmek olmaz!", "Xeta", MessageBoxButtons.OK);
+                            break;
+                        }
+                        else
+                        {
+                            lblResult.Text = ((double)number01 / number02).ToString();
+                        }
+                        break;
+                    default:
+                        MessageBox.Show("Gozlenilmez xeta bas verdi!", "Xeta", MessageBoxButtons.OK);
+                        break;
+                }
+                int locationX = x - lblResult.Size.Width;
+                int locationY = lblResult.Location.Y;
+                lblResult.Location = new Point(locationX, locationY);
+            }
+        }
+
+        private void cmbOperations_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int locationX = nudNumber01.Location.X + nudNumber01.Size.Width / 2 - 10;
+            int locationY = nudNumber01.Location.Y + nudNumber01.Size.Height + (nudNumber02.Location.Y - nudNumber01.Size.Height - nudNumber01.Location.Y) / 2 - lblOperation.Size.Height / 2;
+            lblOperation.Location = new Point(locationX, locationY);
+            lblOperation.Visible = true;
+            lblOperation.Text = cmbOperations.SelectedItem.ToString();
+        }
+    }
+}
