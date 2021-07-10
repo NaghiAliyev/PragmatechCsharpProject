36c773c249047649e4a935fc840c1dc116ee21e1
Naghi Aliyev <naghi.aliyev@gmail.com>
2021-07-10 08:48:18 +0400

    Week05 Task05 is done

---
+++ Tasks/WeeklyTasks/Week05/Task05/Task05/Main.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace Task05
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvMembers = new System.Windows.Forms.ListView();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbSurname = new System.Windows.Forms.TextBox();
+            this.dtpBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.mtbPhoneNumber = new System.Windows.Forms.MaskedTextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chSurname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chPhoneNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chBirthDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvMembers
+            // 
+            this.lvMembers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chSurname,
+            this.chPhoneNumber,
+            this.chBirthDate});
+            this.lvMembers.HideSelection = false;
+            this.lvMembers.Location = new System.Drawing.Point(55, 203);
+            this.lvMembers.Name = "lvMembers";
+            this.lvMembers.Size = new System.Drawing.Size(786, 240);
+            this.lvMembers.TabIndex = 5;
+            this.lvMembers.UseCompatibleStateImageBehavior = false;
+            this.lvMembers.View = System.Windows.Forms.View.Details;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(111, 41);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(282, 27);
+            this.tbName.TabIndex = 0;
+            // 
+            // tbSurname
+            // 
+            this.tbSurname.Location = new System.Drawing.Point(111, 93);
+            this.tbSurname.Name = "tbSurname";
+            this.tbSurname.Size = new System.Drawing.Size(282, 27);
+            this.tbSurname.TabIndex = 1;
+            // 
+            // dtpBirthDate
+            // 
+            this.dtpBirthDate.Location = new System.Drawing.Point(578, 90);
+            this.dtpBirthDate.Name = "dtpBirthDate";
+            this.dtpBirthDate.Size = new System.Drawing.Size(280, 27);
+            this.dtpBirthDate.TabIndex = 3;
+            // 
+            // mtbPhoneNumber
+            // 
+            this.mtbPhoneNumber.Location = new System.Drawing.Point(578, 38);
+            this.mtbPhoneNumber.Mask = "(999) 000-00-00";
+            this.mtbPhoneNumber.Name = "mtbPhoneNumber";
+            this.mtbPhoneNumber.Size = new System.Drawing.Size(280, 27);
+            this.mtbPhoneNumber.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(31, 44);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(46, 21);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Ad : ";
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(31, 96);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(70, 21);
+            this.lblSurname.TabIndex = 5;
+            this.lblSurname.Text = "Soyad : ";
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(434, 41);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(138, 21);
+            this.lblPhoneNumber.TabIndex = 5;
+            this.lblPhoneNumber.Text = "Telefon Nömrəsi :";
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.Location = new System.Drawing.Point(450, 95);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(122, 21);
+            this.lblBirthDate.TabIndex = 5;
+            this.lblBirthDate.Text = "Doğum Tarixi : ";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(767, 147);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(91, 41);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Elave et";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // chName
+            // 
+            this.chName.Text = "Ad";
+            // 
+            // chSurname
+            // 
+            this.chSurname.Text = "Soyad";
+            // 
+            // chPhoneNumber
+            // 
+            this.chPhoneNumber.Text = "Telefon Nömrəsi";
+            // 
+            // chBirthDate
+            // 
+            this.chBirthDate.Text = "Doğum Tarixi";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 472);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblBirthDate);
+            this.Controls.Add(this.lblPhoneNumber);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.mtbPhoneNumber);
+            this.Controls.Add(this.dtpBirthDate);
+            this.Controls.Add(this.tbSurname);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.lvMembers);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvMembers;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbSurname;
+        private System.Windows.Forms.DateTimePicker dtpBirthDate;
+        private System.Windows.Forms.MaskedTextBox mtbPhoneNumber;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chSurname;
+        private System.Windows.Forms.ColumnHeader chPhoneNumber;
+        private System.Windows.Forms.ColumnHeader chBirthDate;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ Tasks/WeeklyTasks/Week05/Task05/Task05/Main.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task05
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        // Global Variables
+        string name;
+        string surname;
+        string phoneNumber;
+        string birthDate;
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            resizeLVColumns();
+        }
+
+        // Method for column sizing
+        private void resizeLVColumns()
+        {
+            int headerWidth = (lvMembers.Width-4) / lvMembers.Columns.Count;
+            foreach (ColumnHeader column in lvMembers.Columns)
+            {
+                column.Width = headerWidth;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            name = tbName.Text.Trim();
+            surname = tbSurname.Text.Trim();
+            phoneNumber = mtbPhoneNumber.Text;
+            birthDate = dtpBirthDate.Value.ToString("MM-dd-yyyy");
+
+            ListViewItem member = new ListViewItem(name);
+            member.SubItems.Add(surname);
+            member.SubItems.Add(phoneNumber);
+            member.SubItems.Add(birthDate);
+            if (checkMember(member))
+            {
+                lvMembers.Items.Add(member);
+            }
+            else
+            {
+                MessageBox.Show("Daxil etdiyiniz melumatlara sahib bir istifadeci var!");
+            }
+        }
+
+        // Method for member checking
+        private bool checkMember(ListViewItem item)
+        {
+            foreach (ListViewItem member in lvMembers.Items)
+            {
+                bool f = true;
+                for (int i = 0; i < member.SubItems.Count; i++)
+                {
+                    if (member.SubItems[i].ToString() != item.SubItems[i].ToString())
+                    {
+                        f = false;
+                        break;
+                    }
+                }
+                if (f)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
